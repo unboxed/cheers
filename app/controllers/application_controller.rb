9e88be32378da760b403d35577e6406e2d70747b
Cale Tilford <combinekidd@gmail.com>
2015-07-01 15:49:57 +0100

    Implement shoutout undo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def shoutout
+    return undo if params[:text].strip == 'undo'
     if (recipient = params[:text].match(/^@\w+/)).nil?
       return render text: "Please supply a username (with @)"
     else
@@ -21,5 +22,14 @@ class ApplicationController < ActionController::Base
     )
 
     render text: "You sent #{shoutout.message} to #{recipient}"
+  end
+
+  def undo
+    Shoutout.where(sender: params[:user_name])
+      .order('created_at DESC')
+      .limit(1)
+      .first
+      .delete
+    render text: "Shoutout undone!"
   end
 end
