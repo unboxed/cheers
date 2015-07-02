7be39934220d4ff286e442857d8db48f39217cbf
Charlie Egan <charlie.egan@unboxedconsulting.com>
2015-07-02 17:16:37 +0100

    Add http_basic_authenticate_with for /admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: 'cheerybot', password: 'share the love', only: :admin
   protect_from_forgery with: :null_session
 
   def index
