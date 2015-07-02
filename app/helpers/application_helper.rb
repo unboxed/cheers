d4d6fb3a489e6d14830a8564b0803cf12f1bd530
Charlie Egan <charlie.egan@unboxedconsulting.com>
2015-07-02 13:52:40 +0100

    Revise shoutout list styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def highlight(message, names)
+    message.tap do |message|
+      names.each do |name|
+        message.gsub!("@#{name}", "<mark>@#{name}</mark>")
+      end
+    end
+  end
+
+  def time_ago_message(time)
+    (distance_of_time_in_words(time, Time.zone.now))
+      .gsub('about', '')
+      .gsub('less than a', '1')
+      .gsub(/ minutes?/, 'm')
+      .gsub(/ hours?/, 'h')
+      .gsub(/ days?/, 'd')
+      .strip + ' ago'
+  end
 end
