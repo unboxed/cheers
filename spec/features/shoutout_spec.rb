5fb5329a3d008b279c8cf3e8130f98204d574707
Cale Tilford <combinekidd@gmail.com>
2015-07-06 12:02:58 +0100

    Add expectation for shoutout response.

--- spec/features/shoutout_spec.rb
+++ spec/features/shoutout_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.feature "Shouting out to another person" do
 
   specify "displays the shoutout message, and time on the home page" do
-    send_shoutout(from: "Jeff", message: "thanks @Bob for all the fish")
+    response = send_shoutout(from: "Jeff", message: "thanks @Bob for all the fish")
     visit root_path
     expect(page).to have_content "thanks @Bob for all the fish"
     expect(page).to have_content "Jeff made a shoutout to Bob"
     expect(page).to have_content "1m ago"
+    expect(response.body).to eq "Shoutout to Bob saved!"
   end
 end
 
