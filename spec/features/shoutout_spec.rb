0ac6306a3eca91e3b236a379fac0253ff1970c7b
Cale Tilford <combinekidd@gmail.com>
2015-07-06 12:27:31 +0100

    Add shoutout undo feature spec.

--- spec/features/shoutout_spec.rb
+++ spec/features/shoutout_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.feature "Shouting out to another person" do
-
   specify "displays the shoutout message, and time on the home page" do
     response = send_shoutout(from: "Jeff", message: "thanks @Bob for all the fish")
     visit root_path
@@ -9,6 +8,16 @@ RSpec.feature "Shouting out to another person" do
     expect(page).to have_content "Jeff made a shoutout to Bob"
     expect(page).to have_content "1m ago"
     expect(response.body).to eq "Shoutout to Bob saved!"
+  end
+end
+
+RSpec.feature "Undoing previous shoutout" do
+  specify "undoes the shoutout message" do
+    create(:shoutout, sender: "Jeff", message: "thx to @Bob", recipients: ["Bob"])
+    response = send_shoutout(from: "Jeff", message: "undo")
+    visit root_path
+    expect(page).to_not have_content "thx to @Bob"
+    expect(response.body).to eq "Shoutout undone!"
   end
 end
 
