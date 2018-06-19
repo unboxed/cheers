50b49dacbadbc2d332f906ad00667d39bc9fd911
Celia Collins <celiadcollins@gmail.com>
2018-06-19 17:44:27 +0100

    Ping the user directly when they have received a shoutout   - Added post_direct_message to slack_service   - This is called in shoutouts_controller

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -2,7 +2,17 @@ class SlackService
   def initialize(client = Slack::Client.new)
     @client = client
   end
+
   def post_message(channel, message)
     @client.chat_postMessage(channel: channel, text: message, as_user: true)
+  end
+
+  def post_direct_message(token, users, message)
+    members = @client.users_list(token: token)
+    user_id_array = []
+    members["members"].each do |key, value|
+      users.each { |user| user_id_array << key["id"] if key["name"] == user }
+    end
+    user_id_array.each { |id| post_message(id, message) }
   end
 end
